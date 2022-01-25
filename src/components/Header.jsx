cf170a0b866221b66f7ea5d7c34db6ba946aebc0
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-25 11:51:43 -0400

    [FEATURE][HEADER] add hook useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
 
+  const color = useContext(ThemeContext);
+
   const handleClick = () => {
-      setDarkMode(!darkMode)
-  }
+    setDarkMode(!darkMode);
+  };
 
   return (
     <div className="Header">
-      <h1>ReactHooks</h1>
-      <button type="button" onClick={handleClick}>{ darkMode ? "Dark Mode" : "Light Mode"}</button>
+      <h1 style={{ color }}>ReactHooks</h1>
+      <button type="button" onClick={handleClick}>
+        {darkMode ? "Dark Mode" : "Light Mode"}
+      </button>
     </div>
   );
 };
