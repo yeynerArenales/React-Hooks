ec74cd552e2e47677f49027e9d1d9d70095a0b72
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-25 11:18:32 -0400

    [FEATURE][CHARACTERS] get info for api

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-export const Characters = () => {
+const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
+
   return (
-      <div className='Characters'>
-          <h2>{}</h2>
-      </div>
+    <div className="Characters">
+      {characters.map((character) => (
+        <h2>{character.name}</h2>
+      ))}
+    </div>
   );
 };
+
+export default Characters;
