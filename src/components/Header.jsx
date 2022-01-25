5ee4367e8b1e8bb189b7e0027e3babd024ebfc6e
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-25 10:57:32 -0400

    [FEATURE][HEADER] create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = () => {
+      setDarkMode(!darkMode)
+  }
+
+  return (
+    <div className="Header">
+      <h1>ReactHooks</h1>
+      <button type="button" onClick={handleClick}>{ darkMode ? "Dark Mode" : "Light Mode"}</button>
+    </div>
+  );
+};
+
+export default Header;
